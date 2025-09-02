b391a9fa330468c59db6610b78642b2a5a030b6c
Informeskaipore <informes@gimnasiokaipore.com>
2025-09-01 19:22:14 -0500

    Create db_utils.py
    Datos de conexion a base de datos

---
+++ db_utils.py
@@ -0,0 +1,12 @@
+# db_utils.py
+import streamlit as st
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host=st.secrets["mysql"]["host"],
+        port=st.secrets["mysql"]["port"],
+        user=st.secrets["mysql"]["user"],
+        password=st.secrets["mysql"]["password"],
+        database=st.secrets["mysql"]["database"]
+    )
